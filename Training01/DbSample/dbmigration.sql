d60f25e64cf607e88a34aeb6c4c287bd27b2d63a
Frank Eller <fe@frankeller.de>
2019-10-04 10:37:48 +0200

    DELETED: Removed old sample applications

--- Training01/DbSample/dbmigration.sql
+++
@@ -1,9 +0,0 @@
-﻿ALTER TABLE [TodoItems] ADD [Description] nvarchar(max) NULL;
-
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20190416084359_migv2', N'2.2.3-servicing-35854');
-
-GO
-
